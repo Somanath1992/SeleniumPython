234354d113956f701f8047724f11041f364843db
Somanath1992 <somanathjadhav1992@gmail.com>
2024-07-05 18:38:57 +0530

    Comment line for auth pop landing page

--- Working With WebElements/authentication_pop_up.py
+++ Working With WebElements/authentication_pop_up.py
@@ -8,5 +8,6 @@ driver = webdriver.Chrome(service=service)
 driver.maximize_window()
 driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth")
 driver.implicitly_wait(10)
+# To verify Login is Successful
 landing_page_text = driver.find_element(By.XPATH, "//div[@class='example']")
 print(landing_page_text.text)
